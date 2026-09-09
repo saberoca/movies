a3aa98ecca664dd54b89cae18b9d2a788ffa96b9
Santos Bertoni <saberoca@gmail.com>
2026-09-08 18:40:41 -0600

    Firs commit with documentation

---
+++ src/main/java/com/saints/movies/common/ApiConstants.java
@@ -0,0 +1,24 @@
+package com.saints.movies.common;
+
+public class ApiConstants {
+
+    //documentación controladores
+    // Movie
+    public static final String MOVIE_TAG = "Movie";
+    public static final String MOVIE_DESCRIPTION = "Operaciones sobre peliculas";
+
+    //documentación respuestas
+    public static final String SUCCESSFUL = "200";
+    public static final String SUCCESSFUL_MESSAGE = "Successful";
+    public static final String CREATED = "201";
+    public static final String CREATED_MESSAGE = "Recurso creado exitosamente";
+    public static final String NO_CONTENT = "204";
+    public static final String NO_CONTENT_MESSAGE = "Operación exitosa sin contenido";
+    public static final String BAD_REQUEST = "400";
+    public static final String BAD_REQUEST_MESSAGE = "Bad request";
+    public static final String RESOURCE_NOT_FOUND = "404";
+    public static final String RESOURCE_NOT_FOUND_MESSAGE = "Resource not found";
+    public static final String INTERNAL_ERROR = "500";
+    public static final String INTERNAL_ERROR_MESSAGE = "Internal error";
+
+}
--- src/main/java/com/saints/movies/movie/controller/MovieController.java
+++ src/main/java/com/saints/movies/movie/controller/MovieController.java
@@ -1,8 +1,13 @@
 package com.saints.movies.movie.controller;
 
+import com.saints.movies.common.ApiConstants;
 import com.saints.movies.movie.dto.MovieRequest;
 import com.saints.movies.movie.dto.MovieResponse;
 import com.saints.movies.movie.service.MovieService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,11 +20,18 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/movies")
 @RestController
+@Tag(name = ApiConstants.MOVIE_TAG, description = ApiConstants.MOVIE_DESCRIPTION)
 public class MovieController {
 
     private final MovieService movieService;
 
     @GetMapping
+    @Operation(summary = "Busca peliculas con filtro opcional", description = "Busca todas las peliculas con filtro opcional filtro = {genero, titulo, año}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = ApiConstants.SUCCESSFUL, description = ApiConstants.SUCCESSFUL_MESSAGE),
+            @ApiResponse(responseCode = ApiConstants.RESOURCE_NOT_FOUND, description = ApiConstants.RESOURCE_NOT_FOUND_MESSAGE),
+            @ApiResponse(responseCode = ApiConstants.INTERNAL_ERROR, description = ApiConstants.INTERNAL_ERROR_MESSAGE)
+    })
     public ResponseEntity<List<MovieResponse>> getAllMovies(@RequestParam(required = false) String genre,
                                                             @RequestParam(required = false) String title,
                                                             @RequestParam(required = false) Integer year){
@@ -27,24 +39,50 @@ public class MovieController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Busca pelicula por id", description = "Busca una pelicula por el id de la pelicula")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = ApiConstants.SUCCESSFUL, description = ApiConstants.SUCCESSFUL_MESSAGE),
+            @ApiResponse(responseCode = ApiConstants.RESOURCE_NOT_FOUND, description = ApiConstants.RESOURCE_NOT_FOUND_MESSAGE),
+            @ApiResponse(responseCode = ApiConstants.INTERNAL_ERROR, description = ApiConstants.INTERNAL_ERROR_MESSAGE)
+    })
     public ResponseEntity<MovieResponse> getMovieById(@PathVariable Long id){
         return ResponseEntity.ok(movieService.getMovieById(id));
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
+    @Operation(summary = "Crear pelicula", description = "Crear una nueva pelicula")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = ApiConstants.CREATED, description = ApiConstants.CREATED_MESSAGE),
+            @ApiResponse(responseCode = ApiConstants.BAD_REQUEST, description = ApiConstants.BAD_REQUEST_MESSAGE),
+            @ApiResponse(responseCode = ApiConstants.RESOURCE_NOT_FOUND, description = ApiConstants.RESOURCE_NOT_FOUND_MESSAGE),
+            @ApiResponse(responseCode = ApiConstants.INTERNAL_ERROR, description = ApiConstants.INTERNAL_ERROR_MESSAGE)
+    })
     public ResponseEntity<MovieResponse> createMovie(@Valid @RequestBody MovieRequest request){
         return ResponseEntity.status(HttpStatus.CREATED).body(movieService.createMovie(request));
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
+    @Operation(summary = "Actualizar pelicula", description = "Actualizar una pelicula buscando por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = ApiConstants.SUCCESSFUL, description = ApiConstants.SUCCESSFUL_MESSAGE),
+            @ApiResponse(responseCode = ApiConstants.BAD_REQUEST, description = ApiConstants.BAD_REQUEST_MESSAGE),
+            @ApiResponse(responseCode = ApiConstants.RESOURCE_NOT_FOUND, description = ApiConstants.RESOURCE_NOT_FOUND_MESSAGE),
+            @ApiResponse(responseCode = ApiConstants.INTERNAL_ERROR, description = ApiConstants.INTERNAL_ERROR_MESSAGE)
+    })
     public ResponseEntity<MovieResponse> updateMovie(@Valid @RequestBody MovieRequest request, @PathVariable Long id){
         return ResponseEntity.ok(movieService.updateMovie(id, request));
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar pelicula", description = "Eliminar pelicula por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = ApiConstants.NO_CONTENT, description = ApiConstants.NO_CONTENT_MESSAGE),
+            @ApiResponse(responseCode = ApiConstants.RESOURCE_NOT_FOUND, description = ApiConstants.RESOURCE_NOT_FOUND_MESSAGE),
+            @ApiResponse(responseCode = ApiConstants.INTERNAL_ERROR, description = ApiConstants.INTERNAL_ERROR_MESSAGE)
+    })
     public ResponseEntity<Void> deleteMovie(@PathVariable Long id){
         movieService.deleteMovie(id);
         return ResponseEntity.noContent().build();
